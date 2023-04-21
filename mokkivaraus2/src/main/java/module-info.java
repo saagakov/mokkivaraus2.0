10cc7ea7a1c3f36afe3d92984257ec567afb10b7
saagakov <130475261+saagakov@users.noreply.github.com>
2023-04-21 20:04:34 +0300

    Delete mokkivaraus2 directory

--- mokkivaraus2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.mokkivaraus2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.mokkivaraus2 to javafx.fxml;
-    exports com.example.mokkivaraus2;
-}
